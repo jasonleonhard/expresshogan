f3bfb2dc85c96a483910a110b7968498b83a4e37
un5t0ppab13 <devbrights@gmail.com>
2014-10-15 14:18:45 -0700

    notes

--- hello5.js
+++ hello5.js
@@ -1,23 +1,23 @@
-var fs = require('fs');                     //How we require modules
-var http = require('http');                     //How we require modules
+var fs = require('fs');            //How we require modules
+var http = require('http');        //How we require modules
 
 http.createServer(function(request, response) { // REQUEST EVENT (readable stream, writeable stream)
   var newFile = fs.createWriteStream("readme_copy.md");
-  var fileBytes = request.headers['content-length'];
-  var uploadedBytes = 0;
+  var fileBytes = request.headers['content-length'];   // store content length in fileBytes
+  var uploadedBytes = 0; // keep track of uploaded Bytes so far
 
   request.pipe(newFile);       // pipe read stream to write stream
 
-  request.on('data', function(chunk) {
-    uploadedBytes += chunk.length;
-    var progress = (uploadedBytes / fileBytes) * 100;
-    response.write("progress: " + parseInt(progress, 10) + "%\n");
+  request.on('data', function(chunk) {   // listen to data event that gets called every time we get a new chunk from the request
+    uploadedBytes += chunk.length;  // increase number of uploaded bytes, get length and add to uploaded bytes
+    var progress = (uploadedBytes / fileBytes) * 100; // calc total progress 
+    response.write("progress: " + parseInt(progress, 10) + "%\n"); // write progress back to response
   });
 
-  request.on('end', function(){
-    response.end('uploaded!');
+  request.on('end', function(){  // listen to end event to know when to end response
+    response.end('uploaded!');   // send string back to client
   });
-}).listen(8080);
+}).listen(8080);                 // Port listening to 8080
 
 //Directions
   // node hello5.js
