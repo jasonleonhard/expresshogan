1b858f22d269cbba776a89a1331aaaf1b94d3495
un5t0ppab13 <devbrights@gmail.com>
2014-10-15 12:17:56 -0700

    new routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = exports = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'Express Hogan App Sweet!' });    // index.hjs rendered
+});
+
+
+// EASILY ADD NEW ROUTES BELOW
+// Example: show some json
+router.get('/dolce', function(req, res) {
+  res.send({name: 'Dolce', age:4});
+});
+
+router.get('/api', function(req, res) {
   res.render('index', { title: 'Express Hogan App' });    // index.hjs rendered
 });
 
-// attempt to do soemthing ... show some json
-router.get('/api', function(req, res) {
-  res.send({name: 'Dolce', age:4});
+router.get('/home', function(req, res) {
+  res.send('home!');
 });
+
+
+
 
 
 module.exports = router;
 
-
+//require('./user');
