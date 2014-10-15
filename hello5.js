ce45eff4ae3bfa948c907c07f5b45d1e09b56afb
un5t0ppab13 <devbrights@gmail.com>
2014-10-15 14:49:28 -0700

    notes

--- hello5.js
+++ hello5.js
@@ -1,15 +1,36 @@
-var fs = require('fs');            //How we require modules
-var http = require('http');        //How we require modules
+// node.js is not a framework (not like rails)
+  // there are libraries for that though...
+// it is low level and ultimatly C code
+  // single threaded server, but nonblocking code (with streams)
+
+// what node.js can do:  
+  // websocket/chat server, file upload client (nonblocking), ad server, real-time data apps
+
+
+
+                                                // cannot include -g modules, must install locally 1st
+                                                  //npm install coffee-script -g      // if module has an executable must use -g
+var coffee = require('coffee-script');
+                                                  //coffee app.coffee
+
+
+
+
+
+
+//if you were to install a module          ie     npm install fs
+var fs = require('fs');                         //How we require module fs    ie   fs.js   // if module is not already included must be installed
+var http = require('http');                     //How we require module http        
 
 http.createServer(function(request, response) { // REQUEST EVENT (readable stream, writeable stream)
-  var newFile = fs.createWriteStream("readme_copy.md");
+  var newFile = fs.createWriteStream("readme_copy.md");//
   var fileBytes = request.headers['content-length'];   // store content length in fileBytes
-  var uploadedBytes = 0; // keep track of uploaded Bytes so far
+  var uploadedBytes = 0;                        // keep track of uploaded Bytes so far
 
-  request.pipe(newFile);       // pipe read stream to write stream
+  request.pipe(newFile);                        // pipe read stream to write stream
 
-  request.on('data', function(chunk) {   // listen to data event that gets called every time we get a new chunk from the request
-    uploadedBytes += chunk.length;  // increase number of uploaded bytes, get length and add to uploaded bytes
+  request.on('data', function(chunk) {          // listen to data event that gets called every time we get a new chunk from the request
+    uploadedBytes += chunk.length;              // increase number of uploaded bytes, get length and add to uploaded bytes
     var progress = (uploadedBytes / fileBytes) * 100; // calc total progress 
     response.write("progress: " + parseInt(progress, 10) + "%\n"); // write progress back to response
   });
@@ -28,3 +49,15 @@ http.createServer(function(request, response) { // REQUEST EVENT (readable strea
 // google chromes -> real-time web
 
 
+
+
+
+/*
+ useful cmd ln 
+   npm search request
+
+
+node-toolbox
+npm registery
+
+*/
